ca59c60b3d4d41bffe839f8cead474b784b85aa6
Mkefss <Mauricio.291202@gmail.com>
2021-06-16 21:22:48 -0500

    Optimizado

--- app/src/main/java/com/carlosnolazco/ayudamediosito/AudioModel.java
+++ app/src/main/java/com/carlosnolazco/ayudamediosito/AudioModel.java
@@ -1,19 +1,6 @@
 package com.carlosnolazco.ayudamediosito;
 
 public class AudioModel {
-    String aPath;
-    String aName;
-
-    public String getaPath() {
-        return aPath;
-    }
-    public void setaPath(String aPath) {
-        this.aPath = aPath;
-    }
-    public String getaName() {
-        return aName;
-    }
-    public void setaName(String aName) {
-        this.aName = aName;
-    }
+    public String path;
+    public String name;
 }
--- app/src/main/java/com/carlosnolazco/ayudamediosito/MainActivity.java
+++ app/src/main/java/com/carlosnolazco/ayudamediosito/MainActivity.java
@@ -1,76 +1,41 @@
 package com.carlosnolazco.ayudamediosito;
 
 import android.Manifest;
-import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
-import android.util.Log;
-import android.view.View;
-import android.widget.Adapter;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
-import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    // Codido enviado cuando el permiso es concedido
     private final static int PERMISSION_REQUEST = 1;
     ArrayList<AudioModel> musica = new ArrayList<>();
-    //RecyclerView lista = findViewById(R.id.lista);
-
+    ListView mostrarMusica;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ListView mostrarMusica = findViewById(R.id.listaM);
+        mostrarMusica = findViewById(R.id.listaM);
 
-        // Verificar que tenga permiso al almacenamiento
-        if(ContextCompat.checkSelfPermission(this,
-            Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-            // Si no tiene permiso al almacenamiento entonces pedirle al usuario que le de permiso
-            ActivityCompat.requestPermissions(this,
-                new String[] {Manifest.permission.READ_EXTERNAL_STORAGE}, PERMISSION_REQUEST);
-        } else {
-            getAllAudioFromDevice();
-        }
-
-        ArrayAdapter adaptador = new ArrayAdapter(this, android.R.layout.simple_list_item_1,musica);
-        mostrarMusica.setAdapter(adaptador);
-
-        mostrarMusica.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
-
-                String songName=mostrarMusica.getItemAtPosition(position).toString();
-
-
-
-                Intent intent=new Intent(MainActivity.this,reproductor.class);
-                intent.putExtra("mySongName",songName);
-                intent.putExtra("songPos",position);
-                intent.putExtra("allSong",musica);
-                startActivity(intent);
-            }
-        });
+        if(externalStorageEnabled())
+           getAllAudioFromDevice();
+        else
+            requestPermissions(new String[] {
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+                }, PERMISSION_REQUEST);
     }
 
     public void getAllAudioFromDevice() {
-        final List<AudioModel> tempAudioList = new ArrayList<>();
         // Uri de el audio
         Uri uri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
         // Info que recoger de las canciones
@@ -85,44 +50,53 @@ public class MainActivity extends AppCompatActivity {
             MediaStore.Audio.Albums.DEFAULT_SORT_ORDER);
         // Conseguir info
         if (c != null) {
-            System.out.println("We have: " + c.getCount());
             while (c.moveToNext()) {
                 AudioModel audioModel = new AudioModel();
-                String path = c.getString(0);
-                String name = c.getString(1);
+                audioModel.path = c.getString(0);
+                audioModel.name = c.getString(1);
 
-                audioModel.setaName(name);
-                audioModel.setaPath(path);
                 musica.add(audioModel);
-
-				Log.d("PATH: ", path);
-				Log.d("TITLE: ", name);
-
-                tempAudioList.add(audioModel);
             }
             c.close();
+        } else {
+            Toast.makeText(this,
+                "Error al conseguir la musica del dispositovo",
+                Toast.LENGTH_LONG).show();
         }
     }
 
-    // Pedir permiso a el almacenamiento
+    /*
+     * COSAS RELACIONADAS CON EL PERMISO DE
+     * ACCEDER A LOS ARCHIVOS
+     */
+
+    public boolean externalStorageEnabled() {
+        int readPermission = ContextCompat.checkSelfPermission(this,
+            Manifest.permission.READ_EXTERNAL_STORAGE);
+
+        return readPermission != PackageManager.PERMISSION_GRANTED;
+    }
+
     @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           @NonNull String[] permissions,
-                                           @NonNull int[] grantResults) {
+    public void
+    onRequestPermissionsResult(int requestCode,
+                               @NonNull String[] permissions,
+                               @NonNull int[] grantResults) {
         /* Si el permiso es nuestro permiso de almacenamiento entonces
          * verificar que los permisos esten realmente y entonces
          * hacer la consulta de las canciones
          * */
         if (requestCode == PERMISSION_REQUEST) {
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                if (ContextCompat.checkSelfPermission(this,
-                    Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
-                    Toast.makeText(this, "Permiso concedido", Toast.LENGTH_SHORT).show();
-                    getAllAudioFromDevice();
-                }
+            if(externalStorageEnabled()) {
+                Toast.makeText(this, "Permiso concedido",
+                    Toast.LENGTH_LONG).show();
+                getAllAudioFromDevice();
             } else {
-                Toast.makeText(this, "Permiso denegado", Toast.LENGTH_SHORT).show();
-                finish();
+                Toast.makeText(this, "Permiso no concedido",
+                    Toast.LENGTH_LONG).show();
+                requestPermissions(new String[]{
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+                }, PERMISSION_REQUEST);
             }
         }
     }
